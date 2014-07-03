30b77876f44348a86e3c548291adb8f89fc36fef
Yankovsky <YankovskyAndrey@gmail.com>
2014-07-03 08:16:48 +0400

    use ngmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,15 @@
 module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt)
   grunt.initConfig({
-    uglify: {
+    ngmin: {
       'nouislider.min.js': 'nouislider.js'
+    },
+    uglify: {
+      'nouislider.min.js': 'nouislider.min.js'
     }
   })
   grunt.registerTask('default', [
+    'ngmin',
     'uglify'
   ])
 }
